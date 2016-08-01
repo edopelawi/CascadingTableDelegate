6cc67de1b6bfca42142a33952e5db7ffa9d890c2
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-08-01 23:42:08 +0700

    Add init and convenience initializer method for CascadingTableDelegate

--- CascadingTableDelegate/Classes/Protocols/CascadingTableDelegate.swift
+++ CascadingTableDelegate/Classes/Protocols/CascadingTableDelegate.swift
@@ -21,11 +21,57 @@ protocol CascadingTableDelegate: UITableViewDataSource, UITableViewDelegate {
 	var childDelegates: [CascadingTableDelegate] { get set }
 	
 	/**
+	Base initializer for this instance.
+	
+	- parameter index:          `index` value for this instance. May be changed later.
+	- parameter childDelegates: Array of child `CascadingTableDelegate`s.
+	
+	- returns: This class' instance.
+	*/
+	init(index: Int, childDelegates: [CascadingTableDelegate])
+	
+	/**
 	Preparation method that will be called by this instance's parent, normally in the first time.
 	
 	- note: This method could be used for a wide range of purposes, e.g. registering table view cells.
+	- note: If this called manually, it should call this instance child's `prepare(tableView:)` method.
 	
 	- parameter tableView: `UITableView` instance.
 	*/
 	func prepare(tableView tableView: UITableView)
+}
+
+extension CascadingTableDelegate {
+	
+	/**
+	Convenience initializer for this protocol, that will assign `index`es of the passed `childDelegates` and call this instance and its child's `prepare(tableView:)` method.
+	
+	- parameter index:          `index` value for this instance. May be changed later.
+	- parameter childDelegates: Array of child `CascadingTableDelegate`s.
+	- parameter tableView:      `UITableView` optional that will use this instance as delegate and data source later. If passed with valid instance, it will be passed on this instance and its child's `prepare(tableView:)` call.
+	
+	- note: This will call this class / implementer's `init(index:childDelegates:)` method.
+	
+	- warning: As noted on the top, this `init` method will call this instance's and its child's `prepare(tableView:)` method. This might cause multiple `prepare(tableView:)` calls if you call it on your `init(index:childDelegates:)` method.
+	
+	- returns: This class' instance.
+	*/
+	init(index:Int, childDelegates: [CascadingTableDelegate], tableView: UITableView?) {
+		
+		self.init(index: index, childDelegates: childDelegates)
+		
+		populateChildDelegateIndexes()
+		
+		if let tableView = tableView {
+			childDelegates.forEach({ $0.prepare(tableView: tableView)})
+		}
+	}
+	
+	private func populateChildDelegateIndexes() {
+
+		childDelegates.enumerate()
+		.forEach { (arrayIndex, child) in
+			child.index = arrayIndex
+		}
+	}
 }
