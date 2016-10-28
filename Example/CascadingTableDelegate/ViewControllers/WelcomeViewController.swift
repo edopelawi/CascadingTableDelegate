aecc0507954956261a88ae18d25a409579cd17af
Ricardo Pramana Suranta <ricardo.pramana@gmail.co>
2016-10-28 15:11:50 +0700

    Initial commit for WelcomeViewController

---
+++ Example/CascadingTableDelegate/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeViewController.swift
+//  CascadingTableDelegate
+//
+//  Created by Ricardo Pramana Suranta on 10/28/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+	
+	@IBAction func nextButtonTapped(sender: AnyObject) {
+		
+		// TODO: Add navigation code here.
+		print("Next Button tapped!")
+	}
+
+}
