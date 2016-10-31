5d45b1b438b5365f789f7a68d258f2f818349064
Ricardo Pramana Suranta <ricardo.pramana@gmail.co>
2016-10-31 14:10:58 +0700

    Initial commit for DestinationDescriptionCell

---
+++ Example/CascadingTableDelegate/TableViewCells/DestinationDescriptionCell.swift
@@ -0,0 +1,60 @@
+//
+//  DestinationDescriptionCell.swift
+//  CascadingTableDelegate
+//
+//  Created by Ricardo Pramana Suranta on 10/31/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class DestinationDescriptionCell: UITableViewCell {
+
+	@IBOutlet private weak var descriptionLabel: UILabel!
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        descriptionLabel.text = nil
+    }
+	
+	override func prepareForReuse() {
+		super.awakeFromNib()
+		descriptionLabel.text = nil
+	}
+	
+	/**
+	Returns preferred height for displaying this class' instance for passed `displayText`.
+	
+	- parameter displayText: `String` optional that will be displayed.
+	
+	- returns: `CGFloat` value.
+	*/
+	static func preferredHeight(displayText displayText: String?) -> CGFloat {
+		
+		let verticalPadding = CGFloat(22)
+		
+		guard let validText = displayText else {
+			return verticalPadding
+		}
+		
+		let horizontalPadding = CGFloat(30)
+		let screenWidth = UIScreen.mainScreen().bounds.width
+		
+		let textWidth = screenWidth - horizontalPadding
+		let textFont = UIFont.systemFontOfSize(14)
+		
+		let textHeight = validText.displayHeight(width: textWidth, font: textFont)
+		
+		return textHeight + verticalPadding
+	}
+ 
+	
+	/**
+	Configures this instance using passed `description`.
+	
+	- parameter description:	`String` optional.
+	*/
+	func configure(description description: String?) {
+		descriptionLabel.text = description
+	}
+}
