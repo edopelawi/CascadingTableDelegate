f323524b3468276414a24f953a4aa27bbaea34fd
Ricardo Pramana Suranta <ricardo.pramana@gmail.co>
2016-11-01 14:53:46 +0700

    Add viewModel for DestinationInfoSectionDelegate

--- Example/CascadingTableDelegate/SectionTableDelegates/DestinationInfoSectionDelegate.swift
+++ Example/CascadingTableDelegate/SectionTableDelegates/DestinationInfoSectionDelegate.swift
@@ -8,45 +8,83 @@
 
 import Foundation
 import CascadingTableDelegate
+import CoreLocation
+
+protocol DestinationInfoSectionViewModel: class {
+	
+	/// Stores location coordinate.
+	var locationCoordinate: CLLocationCoordinate2D? { get }
+	
+	/// Stores `Dictionary` of `String`s of the location info, where the `key` is the human-readable info type, and the `value` is human-readable info text.
+	var locationInfo: [String: String] { get }
+	
+	/// Executed when any property info of this instance is updated.
+	var infoDataChanged: (Void -> Void)? { get set }
+}
 
 class DestinationInfoSectionDelegate: NSObject {
-
-	enum Row: Int {
-		case Map // TODO: Add more rows here
-		
-		static var allValues: [Row] = [ .Map ]
-		
-		var cellIdentifier: String {
-	
-			switch self {
-			case .Map: return DestinationMapCell.nibIdentifier()
-			}
-		}
-	}
 	
 	var index: Int
 	var childDelegates: [CascadingTableDelegate]
 	weak var parentDelegate: CascadingTableDelegate?
 	
-	convenience override init() {
+	var viewModel: DestinationInfoSectionViewModel? {
+		didSet {
+			configureViewModelObserver()
+		}
+	}
+	
+	private weak var currentTableView: UITableView?
+	
+	convenience init(viewModel: DestinationInfoSectionViewModel? = nil) {
 		self.init(index: 0, childDelegates: [])
+		self.viewModel = viewModel
+		configureViewModelObserver()
 	}
 	
 	required init(index: Int, childDelegates: [CascadingTableDelegate]) {
 		self.index = index
 		self.childDelegates = childDelegates
 	}
+	
+	// MARK: - Private methods
+	
+	private func configureViewModelObserver() {
+		
+		viewModel?.infoDataChanged = { [weak self] in
+			self?.currentTableView?.reloadData()
+		}
+	}
+	
+	
+	private func identifierForRow(row: Int) -> String? {
+		
+		if row == 0 {
+			return DestinationMapCell.nibIdentifier()
+		}
+		
+		if let viewModel = viewModel where row <= viewModel.locationInfo.count {
+			return DestinationInfoCell.nibIdentifier()
+		}
+		
+		return nil
+	}
 }
 
 extension DestinationInfoSectionDelegate: CascadingTableDelegate {
 	
 	func prepare(tableView tableView: UITableView) {
+		currentTableView = tableView
+		registerNibs(tableView: tableView)
+	}
+	
+	private func registerNibs(tableView tableView: UITableView) {
 		
-		Row.allValues.map({ $0.cellIdentifier })
+		[ DestinationMapCell.nibIdentifier(), DestinationInfoCell.nibIdentifier() ]
 		.forEach { identifier in
-			
-			let nib = UINib(nibName: identifier, bundle: nil)
-			tableView.registerNib(nib, forCellReuseIdentifier: identifier)
+				
+				let nib = UINib(nibName: identifier, bundle: nil)
+				tableView.registerNib(nib, forCellReuseIdentifier: identifier)
 		}
 	}
 }
@@ -54,16 +92,23 @@ extension DestinationInfoSectionDelegate: CascadingTableDelegate {
 extension DestinationInfoSectionDelegate: UITableViewDataSource {
 
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return Row.allValues.count
+		
+		let defaultRowCount = 1
+		
+		guard let viewModel = viewModel else {
+			return defaultRowCount
+		}
+		
+		return viewModel.locationInfo.count + defaultRowCount
 	}
 	
 	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		
-		guard let row = Row(rawValue: indexPath.row) else {
+		guard let identifier = identifierForRow(indexPath.row) else {
 			return UITableViewCell()
 		}
 		
-		return tableView.dequeueReusableCellWithIdentifier(row.cellIdentifier, forIndexPath: indexPath)
+		return tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath)
 	}
 }
 
@@ -75,13 +120,23 @@ extension DestinationInfoSectionDelegate: UITableViewDelegate {
 	
 	func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
 		
-		guard let row = Row(rawValue: indexPath.row) else {
+		let rowIdentifier = identifierForRow(indexPath.row)
+		
+		if rowIdentifier == DestinationMapCell.nibIdentifier() {
+			return DestinationMapCell.preferredHeight()
+		}
+		
+		guard let _ = rowIdentifier, let viewModel = viewModel else {
 			return CGFloat.min
 		}
 		
-		switch row {
-		case .Map: return DestinationMapCell.preferredHeight()
-		}
+		let infoTypes = Array(viewModel.locationInfo.keys)
+		let infoRow = indexPath.row - 1
+		
+		let infoType = infoTypes[infoRow]
+		let infoText = viewModel.locationInfo[infoType]
+		
+		return DestinationInfoCell.preferredHeight(infoType: infoType, infoText: infoText)
 	}
 	
 	
@@ -94,6 +149,27 @@ extension DestinationInfoSectionDelegate: UITableViewDelegate {
 	}
 	
 	func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
-		// TODO: Add this method's implementation
+		
+		if let mapCell = cell as? DestinationMapCell,
+			let locationCoordinate = viewModel?.locationCoordinate {
+			
+			mapCell.configure(coordinate: locationCoordinate)
+			return
+		}
+		
+		
+		guard let infoCell = cell as? DestinationInfoCell,
+			let locationInfo = viewModel?.locationInfo
+			where indexPath.row <= locationInfo.count else {
+				return
+		}
+		
+		let infoRow = indexPath.row - 1
+		let infoTypes = Array(locationInfo.keys)
+		
+		let infoType = infoTypes[infoRow]
+		let infoText = locationInfo[infoType]
+		
+		infoCell.configure(infoType: infoType, infoText: infoText)
 	}
 }
