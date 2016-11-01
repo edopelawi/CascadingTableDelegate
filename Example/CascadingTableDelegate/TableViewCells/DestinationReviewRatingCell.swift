b987d7ddf84a701aaf313dc9fdc404ce2751ead5
Ricardo Pramana Suranta <ricardo.pramana@gmail.co>
2016-11-01 20:21:29 +0700

    Intial commit for DestinationReviewRatingCell

---
+++ Example/CascadingTableDelegate/TableViewCells/DestinationReviewRatingCell.swift
@@ -0,0 +1,53 @@
+//
+//  DestinationReviewRatingCell.swift
+//  CascadingTableDelegate
+//
+//  Created by Ricardo Pramana Suranta on 11/1/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class DestinationReviewRatingCell: UITableViewCell {
+
+	@IBOutlet private var starImageViews: [UIImageView]?
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+		configure(rating: 0)
+    }
+	
+	override func prepareForReuse() {
+		self.prepareForReuse()
+		configure(rating: 0)
+	}
+		
+	/// Configures this instance with passed `rating`. Valid values are between 0 - 5. Behaviour for invalid values is undefined.
+	func configure(rating rating: Int) {
+		
+		guard let starImageViews = starImageViews else {
+			return
+		}
+		
+		starImageViews.enumerate()
+		.forEach { index, imageView in
+			
+			let insideRating = index < (rating - 1)
+			let starImage = insideRating ? UIImage.greyStar() : UIImage.yellowStar()
+			
+			imageView.image = starImage
+		}
+	}
+	
+}
+
+extension UIImage {
+	
+	static func greyStar() -> UIImage? {
+		return UIImage(named: "icoStarGrey")
+	}
+	
+	static func yellowStar() -> UIImage? {
+		return UIImage(named: "icoStarYellow")
+	}
+}
