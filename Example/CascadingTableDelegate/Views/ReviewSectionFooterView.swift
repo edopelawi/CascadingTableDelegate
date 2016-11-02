e8b987ffac97c3a79feec828d5f569352ba4e3d0
Ricardo Pramana Suranta <ricardo.pramana@gmail.co>
2016-11-02 12:10:59 +0700

    Initial commit for ReviewSectionFooterView

---
+++ Example/CascadingTableDelegate/Views/ReviewSectionFooterView.swift
@@ -0,0 +1,39 @@
+//
+//  ReviewSectionFooterView.swift
+//  CascadingTableDelegate
+//
+//  Created by Ricardo Pramana Suranta on 11/2/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ReviewSectionFooterView: UIView {
+
+	@IBOutlet private weak var showMoreButton: UIButton!
+	
+	/// This instance's button text.
+	var buttonText: String? {
+		get {
+			return showMoreButton.titleLabel?.text
+		}
+		set {
+			showMoreButton.titleLabel?.text = buttonText
+		}
+	}
+	
+	/// Executed when this instance's button tapped.
+	var onButtonTapped: (Void -> Void)?
+	
+	// MARK: - Public methods
+	
+	/// Prefered height to display this instance.
+	static func preferredHeight() -> CGFloat {
+		return CGFloat(46)
+	}
+	
+	// MARK: - Private methods
+	@IBAction private func showMoreButtonTapped(sender: AnyObject) {
+		onButtonTapped?()
+	}
+}
