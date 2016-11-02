4c00a4011dbde1c3b52deb3f66c66bb1ed0143c7
Ricardo Pramana Suranta <ricardo.pramana@gmail.co>
2016-11-02 11:44:25 +0700

    Make DestinationReviewRatingSectionDelegate has CGFloat.min as its footer height

--- Example/CascadingTableDelegate/SectionTableDelegates/DestinationReviewRatingSectionDelegate.swift
+++ Example/CascadingTableDelegate/SectionTableDelegates/DestinationReviewRatingSectionDelegate.swift
@@ -95,6 +95,10 @@ extension DestinationReviewRatingSectionDelegate: UITableViewDelegate {
 		return SectionHeaderView.preferredHeight()
 	}
 	
+	func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+		return CGFloat.min
+	}
+	
 	func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
 		
 		return DestinationReviewRatingCell.preferredHeight()
