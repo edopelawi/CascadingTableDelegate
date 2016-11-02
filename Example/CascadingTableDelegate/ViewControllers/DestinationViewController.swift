2a8b29296c65a95936ba8ced2568c28ff8e3f4f4
Ricardo Pramana Suranta <ricardo.pramana@gmail.co>
2016-11-02 11:45:44 +0700

    Use DestinationReviewUserSectionDelegate in DestinationViewController

--- Example/CascadingTableDelegate/ViewControllers/DestinationViewController.swift
+++ Example/CascadingTableDelegate/ViewControllers/DestinationViewController.swift
@@ -63,14 +63,13 @@ class DestinationViewController: UIViewController {
 		navigationBar?.barStyle = .Black
 	}
 
-	private func createRootDelegate() {
-		
-		// TODO: Fill up the `childDelegates` later.
+	private func createRootDelegate() {				
 		
 		let childDelegates: [CascadingTableDelegate] = [
 			DestinationHeaderSectionDelegate(viewModel: viewModel),
 			DestinationInfoSectionDelegate(viewModel: viewModel),
-			DestinationReviewRatingSectionDelegate(viewModel: viewModel)
+			DestinationReviewRatingSectionDelegate(viewModel: viewModel),
+			DestinationReviewUserSectionDelegate(viewModel: viewModel)
 		]
 				
 		// TODO: Perhaps we could add a non-indexed initializer later... the index seems irrelevant at this phase.
