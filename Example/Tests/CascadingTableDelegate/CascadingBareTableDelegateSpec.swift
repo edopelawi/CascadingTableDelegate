01da830a1f16d4d806170fc144b482b890c6d8a8
Ricardo Pramana Suranta <ricardo.pramana@gmail.co>
2016-11-02 19:58:53 +0700

    Initial commit for CascadingBareTableDelegate class

---
+++ Example/Tests/CascadingTableDelegate/CascadingBareTableDelegateSpec.swift
@@ -0,0 +1,87 @@
+//
+//  CascadingBareTableDelegateSpec.swift
+//  CascadingTableDelegate
+//
+//  Created by Ricardo Pramana Suranta on 11/2/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import CascadingTableDelegate
+
+class CascadingBareTableDelegateSpec: QuickSpec {
+
+	override func spec() {
+		
+		var bareTableDelegate: CascadingBareTableDelegate!
+		var childDelegates: [CascadingTableDelegateStub]!
+		
+		let expectedIndex = 99
+		
+		beforeEach { 
+			
+			childDelegates = [
+				CascadingTableDelegateBareStub(index: 0, childDelegates: []),
+				CascadingTableDelegateBareStub(index: 0, childDelegates: [])
+			]
+			
+			bareTableDelegate = CascadingBareTableDelegate(
+				index: expectedIndex,
+				childDelegates: childDelegates.map({ $0 as CascadingTableDelegate })
+			)
+		}
+		
+		it("should store the passed `index` from init method as index") { 
+			expect(bareTableDelegate.index).to(equal(expectedIndex))
+		}
+		
+		it("should store the passed `childDelegates` from init method as childDelegates") {
+			
+			if bareTableDelegate.childDelegates.count != childDelegates.count {
+				fail("Something's wrong in CascadingBareTableDelegate's childDelegates assignment.")
+				return
+			}
+			
+			bareTableDelegate.childDelegates.enumerate()
+			.forEach({ index, delegate in
+				expect(delegate).to(beIdenticalTo(childDelegates[index]))
+			})
+		}
+		
+		it("prepare(tableView:) should call its child's corresponding methods") { 
+			
+			let expectedTableView = UITableView()
+			
+			bareTableDelegate.prepare(tableView: expectedTableView)
+			
+			childDelegates.forEach({ childStub in
+				expect(childStub.prepareCalled).to(beTrue())
+				expect(childStub.passedTableViewOnPrepare).to(beIdenticalTo(expectedTableView))
+			})
+		}
+		
+		it("tableView(_:numberOfRowsInSection:) should return 0") { 
+			
+			let tableView = UITableView()
+			let section = 0
+			
+			let numberOfRows = bareTableDelegate.tableView(tableView, numberOfRowsInSection: section)
+			
+			expect(numberOfRows).to(equal(0))
+		}
+		
+		it("tableView(_:cellForRowAtIndexPath:) should return new UITableViewCell on every call") { 
+			
+			let tableView = UITableView()
+			let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+			
+			let firstCell = bareTableDelegate.tableView(tableView, cellForRowAtIndexPath: indexPath)
+			let secondCell = bareTableDelegate.tableView(tableView, cellForRowAtIndexPath: indexPath)
+			
+			expect(firstCell).toNot(beIdenticalTo(secondCell))
+			
+		}
+	}
+}
